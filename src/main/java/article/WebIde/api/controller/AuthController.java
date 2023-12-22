329371641859a6dfa594ab96e91e91ece384cff9
박제영 <130127934+Zero982@users.noreply.github.com>
2023-12-22 14:44:37 +0900

    Add files via upload

---
+++ src/main/java/article/WebIde/api/controller/AuthController.java
@@ -0,0 +1,54 @@
+package article.WebIde.api.controller;
+
+
+import article.WebIde.api.dto.MemberRequestDto;
+import article.WebIde.api.dto.MemberResponseDto;
+import article.WebIde.api.dto.TokenDto;
+import article.WebIde.api.dto.TokenRequestDto;
+import article.WebIde.api.dto.util.InvalidPasswordException;
+import article.WebIde.api.dto.util.PasswordValidator;
+import article.WebIde.api.service.AuthService;
+import article.WebIde.api.util.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        try {
+            // 비밀번호 유효성 검사
+            PasswordValidator.validatePassword(memberRequestDto.getPassword());
+
+            ResponseEntity<MemberResponseDto> response = authService.signup(memberRequestDto);
+            return ResponseEntity.ok(response.getBody());
+        } catch (InvalidPasswordException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("이메일이 중복되어 사용할 수 없습니다."));
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto){
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto){
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
+
+
+
+
+}
